a0f4753718740b0921afe5071f55df60dd129e9f
gundlach <gundlach@81766f08-6953-0410-b146-ed83aa51d9bd>
2008-08-07 17:20:17 +0000

    Cleaned up pypi desc.  Straight link to MS SDK installer.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='speech',
-      version='0.3.5',
+      version='0.4.0',
       py_modules=['speech'],
       #install_requires=['win32com','pythoncom'],
 
@@ -9,27 +9,63 @@ setup(name='speech',
         "and text-to-speech capabilities.",
 
       long_description="""
-          Allows your Windows python program to:\n
-            * execute a callback when certain phrases are heard\n
-            * execute a callback when any understandable text is heard\n
-            * have different callbacks for different groups of phrases\n
-            * convert text to speech.\n
-          \n
-          For this to work, you must first install pywin32 (download and
-          run the appropriate version from http://tinyurl.com/5jhg29 ) and
-          the Microsoft Speech kit (download and run "SpeechSDK51.exe" from
-          http://tinyurl.com/5m6v2 ).
-          \n
-          Then you can just "import speech" and be on your way!\n
-          \n
-          Please let me know if you like or use this module - it would make
-          my day!
-          """,
+------------
+speech.py
+------------
+
+  Allows your Windows python program to:
+    * execute a callback when certain phrases are heard
+    * execute a callback when any understandable text is heard
+    * have different callbacks for different groups of phrases
+    * convert text to speech.
+
+Example
+=======
+
+  Showing speaking out loud and listening for all recognizable words.
+  ::
+
+    import speech
+    import time
+
+    def callback(phrase, listener):
+        if phrase == "goodbye":
+            listener.stoplistening()
+        speech.say(phrase)
+
+    listener = speech.listenforanything(callback)
+    while listener.islistening():
+        time.sleep(.5)
+
+Requirements
+============
+
+  Requires Windows XP and Python 2.5.
+
+  In addition to easy_installing speech.py, you'll need pywin32 (installer
+  `here <http://tinyurl.com/5ezco9>`__) and the Microsoft Speech kit
+  (installer `here <http://tinyurl.com/zflb>`__).
+
+Resources
+=========
+
+  * Homepage: http://pyspeech.googlecode.com/
+  * Source:
+
+    - Browse at http://code.google.com/p/pyspeech/source/browse/trunk/
+
+    - Get with **svn co http://pyspeech.googlecode.com/svn/trunk/
+      pyspeech-read-only**
+
+  Please let me know if you like or use this module - it would make
+  my day!
+""",
 
       author='Michael Gundlach',
       author_email='gundlach@gmail.com',
       url='http://code.google.com/p/pyspeech/',
-      keywords = "speech recognition text-to-speech text to speech",
+      keywords = "speech recognition text-to-speech text to speech tts"
+          "voice recognition",
 
       classifiers=[
           'Development Status :: 5 - Production/Stable',
